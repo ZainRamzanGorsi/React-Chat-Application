e056c2420e777cffd03e798597ec8cac1f49ec71
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-12 10:17:49 +0500

    Setup Redux Successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    console.log("Props Data ====>",this.props);
+    return(
+      <div>
+        <h1>Zain Ramzan Gorsi</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+
+export default connect(mapStateToProps, null)(App);
---
+++ src/store/reducer/index.js
@@ -0,0 +1,12 @@
+const Initial_State = {
+    user: [
+        {
+            name: "Zain Ramzan",
+            email: "zainramzan7133@gmail.com"
+        }
+    ]
+}
+
+export default (state = Initial_State) => {
+    return state;
+}
