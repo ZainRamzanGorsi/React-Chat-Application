a2d78af94be3399c61210c4b959a4443f530001a
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-12 15:18:24 +0500

    Create Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'fireabse/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+
+};
+firebase.initializeApp(firebaseConfig)
+export default firebase;
