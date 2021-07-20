29d487e424cb4b726f16b95c8bc81e6961fb0801
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-20 12:40:34 +0500

    update firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,15 @@ import firebase from 'fireabse/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCTmkwg26TkTsk3RtEBKVeBu_3dsWweALg",
+  authDomain: "react-chat-application-18822.firebaseapp.com",
+  projectId: "react-chat-application-18822",
+  storageBucket: "react-chat-application-18822.appspot.com",
+  messagingSenderId: "1036475447455",
+  appId: "1:1036475447455:web:f62dca4af62839987d0aaa",
+  measurementId: "G-62J461G4CK"
+};
 
 firebase.initializeApp(firebaseConfig)
 export default firebase;
