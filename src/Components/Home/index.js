efe28bc8cb28db4a1d7645931a2c03d95e947c52
ZainRamzanGorsi <zainramzan7133@gmail.com>
2021-07-24 08:36:14 +0500

    update the components

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,26 +4,29 @@ import set_data from '../../store/action';
 import './style.css'
 
 class App extends React.Component {
-  render() {
-    console.log("Props ===>",this.props);
-    return (
-      <div>
-        <h1>React Chat Application</h1>
-        {/* <button onClick={()=>this.props.set_data()}>Set Data to Redux</button> */}
-        <input type='text' placeholder='Enter Your Name' required/><br/><br/>
-        <button>Start the Chat</button>
-        
-      </div>
-    )
-  }
+    getUserName = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>React Chat Application</h1>
+                <input type='text' placeholder='Enter Your Name' required onChange={(e) => this.getUserName(e)} /><br /><br />
+                <button onClick={()=>this.getUserName()}>Start the Chat</button>
+
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  user: state.user
+    user: state.user
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  set_data: () => dispatch(set_data()),
+    set_data: () => dispatch(set_data()),
 })
 
 
